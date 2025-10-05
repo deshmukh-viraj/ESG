6ab28a205c56250d7d676ef23fc49e4ee53adaf2
Vira Deshmukh <your.email@example.com>
2025-10-05 16:31:00 +0530

    data ingestion from s3

--- src/connections/s3_connection.py
+++ src/connections/s3_connection.py
@@ -2,10 +2,7 @@ import boto3
 import pandas as pd
 import logging
 from io import StringIO
-from src.logger import configure_logger
-
-configure_logger()
-logger = logging.getLogger(__name__)
+from src.logger import logging
 
 class s3_operations:
     def __init__(self, bucket_name:str, aws_access_key: str, aws_secret_key: str, region_name: str ="us-east-1"):
@@ -15,13 +12,13 @@ class s3_operations:
         try:
             self.s3_client = boto3.client(
                 's3',
-                aws_access_key=aws_access_key,
-                aws_secret_key=aws_secret_key,
+                aws_access_key_id=aws_access_key,
+                aws_secret_access_key=aws_secret_key,
                 region_name=region_name
             )
-            logger.info(f"S3 connection initialize for bucket {bucket_name}")
+            logging.info(f"S3 connection initialize for bucket {bucket_name}")
         except Exception as e:
-            logger.exception(f"Failed to initialize S3 connection: {e}")
+            logging.exception(f"Failed to initialize S3 connection: {e}")
             raise
     
     def fetch_file_from_s3(self, file_key: str) -> pd.DataFrame:
@@ -31,11 +28,11 @@ class s3_operations:
         :return Pandas Dataframe
         """
         try:
-            logger.info(f"Fetching file '{file_key}' from s3 bucket '{self.bucket_name}'...")
+            logging.info(f"Fetching file '{file_key}' from s3 bucket '{self.bucket_name}'...")
             obj = self.s3_client.get_object(Bucket=self.bucket_name, Key=file_key)
             df = pd.read_csv(StringIO(obj['Body'].read().decode('utf-8')))
-            logger.info(f"Successfully fetched '{file_key}' from S3 with {len(df)} records.")
+            logging.info(f"Successfully fetched '{file_key}' from S3 with {len(df)} records.")
             return df
         except Exception as e:
-            logger.exception(f"Failed to fetch '{file_key}' from s3: {e}")
+            logging.exception(f"Failed to fetch '{file_key}' from s3: {e}")
             raise
--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -3,30 +3,28 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import yaml
 import logging
-from src.logger import configure_logger
+from src.logger import logging
 from src.connections.s3_connection import s3_operations
 from src.connections import s3_connection
 
-configure_logger()
-logger = logging.getLogger(__name__)
-
+#logger = logging.getLogger(__name__)
 
 def load_params(params_path: str) -> dict:
     """Load parameters from a ymal file"""
     try:
         with open(params_path, 'r') as file:
             params = yaml.safe_load(file)
-        logger.info(f"Parameters loaded from {params_path}")
+        logging.info(f"Parameters loaded from {params_path}")
         return params
     except Exception as e:
-        logger.error(f"Failed to load parameters from {params_path}: {e}")
+        logging.error(f"Failed to load parameters from {params_path}: {e}")
         raise
 
 def load_data(params: dict) -> pd.DataFrame:
     """Load data from a S3"""
     try:
         if all(k in params["data_ingestion"] for k in ("s3_bucket", "aws_access_key", "aws_secret_key", "s3_file_key")):
-            logger.info("Loading data from AWS S3....")
+            logging.info("Loading data from AWS S3....")
             s3 = s3_operations(
                 bucket_name=params["data_ingestion"]["s3_bucket"],
                 aws_access_key=params["data_ingestion"]["aws_access_key"],
@@ -38,16 +36,16 @@ def load_data(params: dict) -> pd.DataFrame:
         
         data_url = params['data_ingestion'].get("data_url")
         if data_url and data_url.startswith("http"):
-            logger.info(f"loading data from URL: {data_url}")
+            logging.info(f"loading data from URL: {data_url}")
             return pd.read_csv(data_url)
 
         local_path = params['data_ingestion'].get("local_path")
         if local_path and os.path.exists(local_path):
-            logger.info("Loading data from local file")
+            logging.info("Loading data from local file")
             return pd.read_csv(local_path)
         raise ValueError("No valid data source found in pramas.yaml")
     except Exception as e:
-        logger.error(f"Failed to laod data: {e}")
+        logging.error(f"Failed to laod data: {e}")
         raise
 
 def preprocess_data(df: pd.DataFrame, params: dict) -> pd.DataFrame:
@@ -56,26 +54,26 @@ def preprocess_data(df: pd.DataFrame, params: dict) -> pd.DataFrame:
         drop_cols = params["data_ingestion"].get("drop_columns", [])
         if drop_cols:
             df.drop(columns=drop_cols, inplace=True, errors='ignore')
-            logger.info(f"Dropped columns: {drop_cols}")
+            logging.info(f"Dropped columns: {drop_cols}")
 
             missing_strategy = params["data_ingestion"].get("handle_missing", "drop")
             if missing_strategy == "drop":
                 df.dropna(inplace=True)
-                logger.info("Dropped rows with missing values")
+                logging.info("Dropped rows with missing values")
             elif missing_strategy == "mean":
                 df.fillna(df.mean(), inplace=True)
-                logger.info("Filled missing values with ")
+                logging.info("Filled missing values with ")
             elif missing_strategy == "median":
                 df.fillna(df.median(), inplace=True)
-                logger.info("Filled missing values with median")
+                logging.info("Filled missing values with median")
             elif missing_strategy == 'mode':
                 df.fillna(df.mode().iloc[0], inplace=True)
-                logger.info("Filled missing values with columns modes")
-            logger.info(f"preprocessing complete")
+                logging.info("Filled missing values with columns modes")
+            logging.info(f"preprocessing complete")
             return df
         
     except Exception as e:
-        logger.error(f"Error During preprocessimg: {e}")
+        logging.error(f"Error During preprocessimg: {e}")
         raise
 
 def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, params: dict) -> None:
@@ -87,10 +85,10 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, params: dict) -
         train_data.to_csv(os.path.join(raw_data_path, "train.csv"), index=False)
         test_data.to_csv(os.path.join(raw_data_path, "test.csv"), index=False)
 
-        logger.info(f"Train and test data saved to {raw_data_path}")
+        logging.info(f"Train and test data saved to {raw_data_path}")
     
     except Exception as e:
-        logger.error(f"Failed to save data: {e}")
+        logging.error(f"Failed to save data: {e}")
         raise
 
 def main():
@@ -101,15 +99,15 @@ def main():
         
 
         #df = load_data(params)
-        s3 = s3_connection.s3_operations("bucket-name", "access_key", "secret_key")
-        df = s3.fetch_file_from_s3("esg.csv")
-        
+        s3 = s3_connection.s3_operations("esg.csv", "AKIAVLMV72KMCAZ6UG73", "JIIMTFvoXqpwOSIBB974EKvlFLlzYvij3yaWUJr1")
+        df = s3.fetch_file_from_s3("company_esg_financial_dataset.csv")
+
         final_df = preprocess_data(df, params)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=random_state)
         save_data(train_data, test_data, params)
     
     except Exception as e:
-        logger.error(f"Data ingestion process failed: {e}")
+        logging.error(f"Data ingestion process failed: {e}")
 
     
 if __name__=="__main__":
